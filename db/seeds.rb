2be4865462cfe2d9e39833a6ea0828dba047ba02
jkaminallen <kaminallenj@gmail.com>
2017-05-11 17:24:03 -0400

    add some css

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+Product.delete_all
+
+Product.create(title: 'Chair',
+  description:
+    %{<p>
+      This is the most comfortable chair you'll ever sit in</p>},
+      image_url: 'chair.jpg',
+      price: 200.99)
